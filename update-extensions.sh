1df30876ab3c0c83221bd249bc9d9eb28c62bcac
Josh <josh@pazmi.no>
2022-10-30 02:17:21 -0400

    Fix update-extensions.sh

--- update-extensions.sh
+++ update-extensions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This shell script updates every subdirectory in extensions/ (or your custom path).
 # Usage: ./update_extensions.sh [extensions_path]
 
@@ -9,18 +11,14 @@ if [ $# -eq 1 ]; then
     EXTENSIONS_PATH=$1
 fi
 
-# Check that the extensions directory exists, and that it contains subfolders
+# Check that the extensions directory exists
 if [ ! -d "$EXTENSIONS_PATH" ]; then
     echo "Extensions directory $EXTENSIONS_PATH does not exist."
     exit 1
 fi
-if [ "$(ls -A $EXTENSIONS_PATH)" ]; then
-    echo "Extensions directory $EXTENSIONS_PATH is empty."
-    exit 1
-fi
 
 # Iterate over all subdirectories of the extensions folder.
-declare –a UPDATED_EXTENSIONS=()
+UPDATED_EXTENSIONS=()
 for dir in $EXTENSIONS_PATH/*/
 do
     echo "Updating $dir"
@@ -31,17 +29,17 @@ do
     NEW_COMMIT=$(git -C $dir rev-parse HEAD)
     # If the commit hash has changed, add the extension to the list of updated extensions, along with its original and updated commit hashes.
     if [ "$CURRENT_COMMIT" != "$NEW_COMMIT" ]; then
-        UPDATED_EXTENSIONS +=("$dir $CURRENT_COMMIT $NEW_COMMIT")
+        UPDATED_EXTENSIONS=("${UPDATED_EXTENSIONS[@]}" "$dir $CURRENT_COMMIT $NEW_COMMIT")
     fi
 done
 
 # If any extensions were updated, list name and commit hashes for each
 if [ ${#UPDATED_EXTENSIONS[@]} -gt 0 ]; then
-    echo "Updated extensions:"
+    echo "\nUpdated extensions:"
     for extension in "${UPDATED_EXTENSIONS[@]}"
     do
         echo "  $extension"
-    else
-        echo "No updates."
     done
+else
+    echo "\nNo updates."
 fi
