e64d8e7e0751827fc86cf46a2bf77cf2679a9609
Josh <josh@pazmi.no>
2022-10-30 01:42:47 -0400

    Improve update-extensions.sh

--- update-extensions.sh
+++ update-extensions.sh
@@ -1,33 +1,47 @@
-# This shell script runs `git pull` in every subdirectory of extensions/ (or your custom folder path).
-
+# This shell script updates every subdirectory in extensions/ (or your custom path).
+# Usage: ./update_extensions.sh [extensions_path]
 
 # Set the path to your extensions folder here.
 EXTENSIONS_PATH="extensions"
 
+# If provided, use the first argument as extensions path
 if [ $# -eq 1 ]; then
     EXTENSIONS_PATH=$1
 fi
 
+# Check that the extensions directory exists, and that it contains subfolders
+if [ ! -d "$EXTENSIONS_PATH" ]; then
+    echo "Extensions directory $EXTENSIONS_PATH does not exist."
+    exit 1
+fi
+if [ "$(ls -A $EXTENSIONS_PATH)" ]; then
+    echo "Extensions directory $EXTENSIONS_PATH is empty."
+    exit 1
+fi
+
 # Iterate over all subdirectories of the extensions folder.
+declare –a UPDATED_EXTENSIONS=()
 for dir in $EXTENSIONS_PATH/*/
 do
     echo "Updating $dir"
-    cd $dir
-    git pull
-    cd ../../
+    # Get the current commit hash.
+    CURRENT_COMMIT=$(git -C $dir rev-parse HEAD)
+    git -C $dir pull
+    # Get the new commit hash.
+    NEW_COMMIT=$(git -C $dir rev-parse HEAD)
+    # If the commit hash has changed, add the extension to the list of updated extensions, along with its original and updated commit hashes.
+    if [ "$CURRENT_COMMIT" != "$NEW_COMMIT" ]; then
+        UPDATED_EXTENSIONS +=("$dir $CURRENT_COMMIT $NEW_COMMIT")
+    fi
 done
 
-# Print a list of results.
-echo "Updated the following extensions:"
-for dir in $EXTENSIONS_PATH/*/
-do
-    # Fetch commit details
-    cd $dir
-    COMMIT_HASH=$(git rev-parse HEAD)
-    COMMIT_DATE=$(git log -1 --format=%cd --date=short)
-    cd ../../
-    # Print the result
-    echo "$(basename $dir) ($COMMIT_HASH, $COMMIT_DATE)"
-done
-
-echo "Done."
+# If any extensions were updated, list name and commit hashes for each
+if [ ${#UPDATED_EXTENSIONS[@]} -gt 0 ]; then
+    echo "Updated extensions:"
+    for extension in "${UPDATED_EXTENSIONS[@]}"
+    do
+        echo "  $extension"
+    else
+        echo "No updates."
+    done
+fi
